cb1a1cf276b057fb9a42f2e7f0ae564242b09294
Muhammad Irgi Fahrezha <2410817210005@mhs.ulm.ac.id>
2026-04-04 00:27:36 +0800

    MODUL 1

---
+++ Praktikum_Web_Modul1/PRAK101.php
@@ -0,0 +1,7 @@
+<?php
+echo "Hello World in PHP";
+echo "<br />";
+echo "Nama Praktikan : Muhammad Irgi Fahrezha";
+echo "<br />";
+echo "NIM : 2410817210005";
+?>
---
+++ Praktikum_Web_Modul1/PRAK102.php
@@ -0,0 +1,11 @@
+<?php
+$Jari_jari = 4.2;
+$Tinggi = 5.4;
+$Panjang = 8.9;
+$lebar = 14.7;
+$Sisi = 7.9;
+
+$volume_bola = (4/3) * M_PI * ($Jari_jari ** 3);
+
+echo "" . number_format($volume_bola, 3, ',', '.') . " m3";
+?>
